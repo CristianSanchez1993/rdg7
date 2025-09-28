d217659d199b1fcb5024f7bdf8d63b6da58d66ee
CristianSanchez1993 <cristianandressanchez.1993@gmail.com>
2025-09-27 23:46:38 -0500

    Se añade analizador estatico de codigo y pruebas unitarias

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -6,30 +6,30 @@ import '../model/user_model.dart';
 import '../resource/constants.dart';
 
 class UserRepository {
+  final http.Client httpClient;
+  UserRepository({http.Client? httpClient}) : httpClient = httpClient ?? http.Client();
   final StreamController<String> _errorController = StreamController<String>();
 
   Stream<String> get errorStream => _errorController.stream;
 
-  Map<String, String> getHeaders() {
-    return {
+  Map<String, String> getHeaders() => {
       HttpHeaders.contentTypeHeader: Constants.contentTypeHeader,
       HttpHeaders.authorizationHeader: Constants.authorizationHeader,
     };
-  }
 
   Future<List<UserModel>> getUsers() async {
     try {
-      final response = await http.get(
-        Uri.parse("${Constants.urlAuthority}/${Constants.userAPIGetAll}"),
+      final response = await httpClient.get(
+        Uri.parse('${Constants.urlAuthority}/${Constants.userAPIGetAll}'),
         headers: getHeaders(),
       );
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> decoded = jsonDecode(response.body);
+        final Map<String, dynamic> decoded = jsonDecode(response.body) as Map<String, dynamic>;
 
         if (decoded.containsKey('data') && decoded['data'] is List) {
-          final List<dynamic> data = decoded['data'];
-          return data.map((item) => UserModel.fromJson(item)).toList();
+          final List<dynamic> data = decoded['data'] as List<dynamic>;
+          return data.map((item) => UserModel.fromJson(item as Map<String, dynamic>)).toList();
         } else {
           _errorController.add(
             "Respuesta inválida: campo 'data' no encontrado o incorrecto.",
@@ -37,11 +37,11 @@ class UserRepository {
         }
       } else {
         _errorController.add(
-          "Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}",
+          'Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}',
         );
       }
     } catch (e) {
-      _errorController.add("Error: $e");
+      _errorController.add('Error: $e');
     }
     return [];
   }
@@ -49,15 +49,15 @@ class UserRepository {
   Future<UserModel?> getUserById(String id) async {
     try {
       final url = Uri.parse(
-        "${Constants.urlAuthority}/${Constants.userAPIGetById}?id=$id",
+        '${Constants.urlAuthority}/${Constants.userAPIGetById}?id=$id',
       );
 
-      final response = await http.get(url, headers: getHeaders());
+      final response = await httpClient.get(url, headers: getHeaders());
 
       if (response.statusCode == 200) {
-        final Map<String, dynamic> decoded = jsonDecode(response.body);
+        final Map<String, dynamic> decoded = jsonDecode(response.body) as Map<String, dynamic>;
         if (decoded.containsKey('data')) {
-          return UserModel.fromJson(decoded['data']);
+          return UserModel.fromJson(decoded['data'] as Map<String, dynamic>);
         } else {
           _errorController.add(
             "Respuesta inválida: campo 'data' no encontrado.",
@@ -65,77 +65,57 @@ class UserRepository {
         }
       } else {
         _errorController.add(
-          "Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}",
+          'Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}',
         );
       }
     } catch (e) {
-      _errorController.add("Error: $e");
+      _errorController.add('Error: $e');
     }
     return null;
   }
 
-  Future<UserModel?> createUser(UserModel user) async {
-    try {
-      final response = await http.post(
-        Uri.parse("${Constants.urlAuthority}/${Constants.userAPICreate}"),
-        headers: getHeaders(),
-        body: jsonEncode(user.toJson()),
-      );
+  Future<UserModel> createUser(UserModel user) async {
+  final response = await httpClient.post(
+    Uri.parse('${Constants.urlAuthority}/${Constants.userAPICreate}'),
+    headers: getHeaders(),
+    body: jsonEncode(user.toJson()),
+  );
 
-      if (response.statusCode == 201 || response.statusCode == 200) {
-        final Map<String, dynamic> decoded = jsonDecode(response.body);
-        if (decoded.containsKey('data')) {
-          return UserModel.fromJson(decoded['data']);
-        } else {
-          _errorController.add(
-            "Respuesta inválida: campo 'data' no encontrado.",
-          );
-        }
-      } else {
-        _errorController.add(
-          "Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}",
-        );
-      }
-    } catch (e) {
-      _errorController.add("Error: $e");
+  if (response.statusCode == 201 || response.statusCode == 200) {
+    final Map<String, dynamic> decoded = jsonDecode(response.body) as Map<String, dynamic>;
+    if (decoded.containsKey('data')) {
+      return UserModel.fromJson(decoded['data'] as Map<String, dynamic>);
+    } else {
+      throw Exception("Respuesta inválida: campo 'data' no encontrado.");
     }
-    return null;
+  } else {
+    throw Exception('Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}');
   }
+}
 
-  Future<UserModel?> updateUser(UserModel user) async {
-    try {
-      final response = await http.put(
-        Uri.parse(
-          "${Constants.urlAuthority}/${Constants.userAPIUpdate}/${user.id}",
-        ),
-        headers: getHeaders(),
-        body: jsonEncode(user.toJson()),
-      );
+  Future<bool> updateUser(UserModel user) async {
+  final response = await httpClient.put(
+    Uri.parse('${Constants.urlAuthority}/${Constants.userAPIUpdate}/${user.id}'),
+    headers: getHeaders(),
+    body: jsonEncode(user.toJson()),
+  );
 
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> decoded = jsonDecode(response.body);
-        if (decoded.containsKey('data')) {
-          return UserModel.fromJson(decoded['data']);
-        } else {
-          _errorController.add(
-            "Respuesta inválida: campo 'data' no encontrado.",
-          );
-        }
-      } else {
-        _errorController.add(
-          "Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}",
-        );
-      }
-    } catch (e) {
-      _errorController.add("Error: $e");
+  if (response.statusCode == 200) {
+    final decoded = jsonDecode(response.body);
+    if (decoded is Map<String, dynamic> && decoded['success'] == true) {
+      return true;
+    } else {
+      throw Exception('Respuesta inesperada: ${response.body}');
     }
-    return null;
+  } else {
+    throw Exception('Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}');
   }
+}
 
   Future<bool> deleteUser(String id) async {
     try {
-      final response = await http.delete(
-        Uri.parse("${Constants.urlAuthority}/${Constants.userAPIDelete}/$id"),
+      final response = await httpClient.delete(
+        Uri.parse('${Constants.urlAuthority}/${Constants.userAPIDelete}/$id'),
         headers: getHeaders(),
       );
 
@@ -143,12 +123,12 @@ class UserRepository {
         return true;
       } else {
         _errorController.add(
-          "Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}",
+          'Error ${response.statusCode}: ${response.reasonPhrase}\n${response.body}',
         );
         return false;
       }
     } catch (e) {
-      _errorController.add("Error: $e");
+      _errorController.add('Error: $e');
       return false;
     }
   }
